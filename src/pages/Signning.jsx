2fcc6345f27eaa2874cd9fdd2544372a0be40843
Menash Fareed <menashfareed@gmail.com>
2023-03-24 05:51:47 -0400

    sign in page swaggy

--- src/pages/Signning.jsx
+++ src/pages/Signning.jsx
@@ -1,13 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useHistory } from "react-router-dom";
 import Button from "../components/Button";
 import Divider from "../components/Divider";
 import Input from "../components/Input";
 import { useAuth } from "../contexts/auth/AuthContext";
-import "../pages/signning";
 
 function Signning() {
-  const { signIn, signInWithGoogle, resetPassword } = useAuth();
+  const { signUp, signIn, signInWithGoogle, resetPassword } = useAuth();
   const history = useHistory();
 
   const initialValues = { email: "", password: "" };
@@ -23,7 +22,7 @@ function Signning() {
     setValues({ ...values, [name]: value });
   };
 
-  const handleSubmit = async (e) => {
+  const handleSignInSubmit = async (e) => {
     e.preventDefault();
 
     const { email, password } = values;
@@ -34,6 +33,26 @@ function Signning() {
     try {
       setLoading(true);
       await signIn(email, password);
+      history.push("/");
+    } catch (error) {
+      setError(error.message);
+    }
+
+    setLoading(false);
+  };
+
+  const handleSignUpSubmit = async (e) => {
+    e.preventDefault();
+
+    const { email, password } = values;
+    if (!email || !password) {
+      return setError("Please fill in all fields");
+    }
+
+    try {
+      setLoading(true);
+      await signUp(email, password);
+
       history.push("/");
     } catch (error) {
       setError(error.message);
@@ -75,65 +94,121 @@ function Signning() {
     setLoading(false);
   };
 
-  return (
-    <main className="lg:max-w-xl lg:p-0 lg:space-y-14 p-6 w-full bg-white space-y-6">
-      <h2>Weekly Coding Challenge #1: Sign in/up Form</h2>
-<div class="container" id="container">
-	<div class="form-container sign-up-container">
-		<form action="#">
-			<h1>Create Account</h1>
-			<div class="social-container">
-				<a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
-				<a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
-				<a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
-			</div>
-			<span>or use your email for registration</span>
-			<input type="text" placeholder="Name" />
-			<input type="email" placeholder="Email" />
-			<input type="password" placeholder="Password" />
-			<button>Sign Up</button>
-		</form>
-	</div>
-	<div class="form-container sign-in-container">
-		<form action="#">
-			<h1>Sign in</h1>
-			<div class="social-container">
-				<a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
-				<a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
-				<a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
-			</div>
-			<span>or use your account</span>
-			<input type="email" placeholder="Email" />
-			<input type="password" placeholder="Password" />
-			<a href="#">Forgot your password?</a>
-			<button>Sign In</button>
-		</form>
-	</div>
-	<div class="overlay-container">
-		<div class="overlay">
-			<div class="overlay-panel overlay-left">
-				<h1>Welcome Back!</h1>
-				<p>To keep connected with us please login with your personal info</p>
-				<button class="ghost" id="signIn">Sign In</button>
-			</div>
-			<div class="overlay-panel overlay-right">
-				<h1>Hello, Friend!</h1>
-				<p>Enter your personal details and start journey with us</p>
-				<button class="ghost" id="signUp">Sign Up</button>
-			</div>
-		</div>
-	</div>
-</div>
+  useEffect(() => {
+    const signUpButton = document.getElementById("signUp");
+    const signInButton = document.getElementById("signIn");
+    const container = document.getElementById("container");
 
-<footer>
-	<p>
-		Created with <i class="fa fa-heart"></i> by
-		<a target="_blank" href="https://florin-pop.com">Florin Pop</a>
-		- Read how I created this and how you can join the challenge
-		<a target="_blank" href="https://www.florin-pop.com/blog/2019/03/double-slider-sign-in-up-form/">here</a>.
-	</p>
-</footer>
-    </main>
+    signUpButton.addEventListener("click", () => {
+      container.classList.add("right-panel-active");
+    });
+
+    signInButton.addEventListener("click", () => {
+      container.classList.remove("right-panel-active");
+    });
+  }, []);
+
+  return (
+    <div class="container" id="container">
+      <div class="form-container sign-up-container">
+        <form action="#">
+          <h1>Create Account</h1>
+          <Input
+            name="email"
+            type="email"
+            placeholder="Enter email address.."
+            value={values.email}
+            handleChange={handleChange}
+          />
+          <Input
+            name="password"
+            type="password"
+            placeholder="Enter password.."
+            value={values.password}
+            handleChange={handleChange}
+          />
+          <Button
+            value="Sign Up"
+            type="submit"
+            variant="primary"
+            action={handleSignUpSubmit}
+            loading={loading}
+            fullWidth
+          />
+          {error && <div className="text-red-600">{error}</div>}
+        </form>
+      </div>
+      <div class="form-container sign-in-container">
+        <form action="#">
+          <h1>Sign in</h1>
+          <Input
+            name="email"
+            type="email"
+            placeholder="Enter email address.."
+            value={values.email}
+            handleChange={handleChange}
+          />
+          <Input
+            name="password"
+            type="password"
+            placeholder="Enter password.."
+            value={values.password}
+            handleChange={handleChange}
+          />
+          <div
+            onClick={handlePassword}
+            className="flex justify-end text-sm text-primary cursor-pointer"
+          >
+            <p>Forgot password?</p>
+          </div>
+          <Button
+            value="Sign In"
+            type="submit"
+            variant="primary"
+            action={handleSignInSubmit}
+            loading={loading}
+            fullWidth
+          />
+          {message && (
+            <div className="text-primary font-semibold">{message}</div>
+          )}
+          {error && <div className="text-red-600">{error}</div>}
+          <Divider text="or" />
+      <Button
+        value="Continue with Google"
+        type="submit"
+        variant="frame"
+        action={handleGoogleSignIn}
+        loading={googleLoading}
+        fullWidth
+      />
+        </form>
+        
+      </div>
+      
+      <div class="overlay-container">
+        <div class="overlay">
+          <div class="overlay-panel overlay-left">
+            <h1 id="overlay-text">Already Better?</h1>
+            <p id="overlay-text">
+              To keep improving with us please login with your personal info
+            </p>
+            <button class="ghost" id="signIn">
+              Sign In
+            </button>
+          </div>
+          <div class="overlay-panel overlay-right">
+            <h1 id="overlay-text">First Time?</h1>
+            <p id="overlay-text">
+              Create an account here and start journey with us
+            </p>
+            <button class="ghost" id="signUp">
+              Sign Up
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
