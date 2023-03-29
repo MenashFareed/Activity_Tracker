b3433088ccff09679d66fa6a48f2a48d86cc52c2
Menash Fareed <menashfareed@gmail.com>
2023-03-29 06:00:56 -0400

    minor fix

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -129,7 +129,7 @@ function Dashboard() {
     <div className="space-y-10 w-full">
       <div className="flex space-x-10 items-end">
         <h1 className="text-4xl">Dashboard</h1>
-        <Link to="/workout">
+        <Link to="/activity">
           <Button value="New Activity" variant="primary" type="text" />
         </Link>
       </div>
